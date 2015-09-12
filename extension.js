fd2e15a30714a6bd170936eb972aa961d89392f1
carrlosdaniel <carlosney_@hotmail.com>
2015-09-12 16:33:09 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,7 +48,7 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "CoolforTheBot",
       language: "portuguese",
-      chatLink: "https://rawgit.com/bscBot/source/master/lang/en.json",
+      chatLink: "https://rawgit.com/bscBot/source/master/lang/pt-BR.json",
       scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
